d03f8b786edea22451534c242465bfc57432b881
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2022-03-01 11:20:47 +0100

    typo..

--- loqusdbapi/models.py
+++ loqusdbapi/models.py
@@ -76,7 +76,7 @@ class Variant(BaseVariant):
 
 
 class StructuralVariant(BaseVariant):
-    id: BsonObjectId = Field(alias="_id")
+    id: Optional[PydanticObjectId] = Field(alias="_id")
     end_chrom: str
     end_left: int
     end_right: int
